94fdf4fa53b67655e6c62dcecd9dade583280f8a
Aaron Cleveland <toscleveland@gmail.com>
2020-10-14 11:22:45 -0500

    Added Circle

--- CustomInterface/ViewController.swift
+++ CustomInterface/ViewController.swift
@@ -8,12 +8,51 @@
 import UIKit
 
 class ViewController: UIViewController {
+    
+    let singleLine = SingleLine()
+    let curveLine = CurveLine()
+    let circlePath = Circle()
+    let rectanglePath = Rectangle()
+    let gradients = Gradients()
 
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view.
+        configureUI()
     }
-
+    
+    func configureUI() {
+//        showSingleLine()
+//        showCurveLine()
+//        showCirclePath()
+        showRectanglePath()
+//        showGradients()
+    }
+    
+    private func showSingleLine() {
+        view.addSubview(singleLine)
+        singleLine.addConstraintsToFillView(view)
+    }
+    
+    private func showCurveLine() {
+        view.addSubview(curveLine)
+        curveLine.addConstraintsToFillView(view)
+    }
+    
+    private func showCirclePath() {
+        view.addSubview(circlePath)
+        circlePath.addConstraintsToFillView(view)
+    }
+    
+    private func showRectanglePath() {
+        view.addSubview(rectanglePath)
+        rectanglePath.addConstraintsToFillView(view)
+    }
+    
+    private func showGradients() {
+        view.addSubview(gradients)
+        gradients.addConstraintsToFillView(view)
+    }
 
 }
 
